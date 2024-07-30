7f6dbde61680ccb9a560ed6311e038939e05732a
betina <yostbetina20@gmail.com>
2024-07-29 23:20:01 -0300

    Terminando de confirgurar MUI

--- mi-proyecto/src/App.jsx
+++ mi-proyecto/src/App.jsx
@@ -5,8 +5,25 @@ import Hero from './component/Hero/Hero';
 import AboutMe from './component/AbotMe/AboutMe';
 import MySkills from './component/MySkills/MySkills';
 import Portfolio from './component/Portfolio/Portfolio';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Contact from './component/Contacts/contact';
 import './App.css';
+
+const theme = createTheme({
+    typography: {
+        h1: {
+            fontSize: '2rem',
+        },
+        h2: {
+            fontSize: '1.5rem',
+        },
+        body1: {
+            fontSize: '1rem',
+        },
+        // Agrega otras configuraciones del tema si es necesario
+    },
+    // Agrega otras configuraciones del tema si es necesario
+});
 
 const App = () => {
     const heroRef = useRef(null);
@@ -17,30 +34,32 @@ const App = () => {
   
 
     return (
-        <Box className="app-container">
-            <Header 
-                heroRef={heroRef}
-                aboutMeRef={aboutMeRef}
-                mySkillsRef={mySkillsRef}
-                portfolioRef={portfolioRef}
-                contactRef={contactRef}
-            />
-            <Box id="inicio" ref={heroRef}>
-                <Hero />
+        <ThemeProvider theme={theme}>
+            <Box className="app-container">
+                <Header 
+                    heroRef={heroRef}
+                    aboutMeRef={aboutMeRef}
+                    mySkillsRef={mySkillsRef}
+                    portfolioRef={portfolioRef}
+                    contactRef={contactRef}
+                />
+                <Box id="inicio" ref={heroRef}>
+                    <Hero />
+                </Box>
+                <Box id="about" ref={aboutMeRef}>
+                    <AboutMe />
+                </Box>
+                <Box id="experience" ref={mySkillsRef}>
+                    <MySkills />
+                </Box>
+                <Box id="portfolio" ref={portfolioRef}>
+                    <Portfolio />
+                </Box>
+                <Box id="contact" ref={contactRef}>
+                    <Contact />
+                </Box>
             </Box>
-            <Box id="about" ref={aboutMeRef}>
-                <AboutMe />
-            </Box>
-            <Box id="experience" ref={mySkillsRef}>
-                <MySkills />
-            </Box>
-            <Box id="portfolio" ref={portfolioRef}>
-                <Portfolio />
-            </Box>
-            <Box id="contact" ref={contactRef}>
-                <Contact />
-            </Box>
-        </Box>
+        </ThemeProvider>
     );
 };
 
